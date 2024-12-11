8408e239aa0ae3584a838cc3638d15de74892902
dane <danerieber@gmail.com>
2024-12-11 14:00:32 -0600

    remove shell completion

--- build-go-sub-package.nix
+++ build-go-sub-package.nix
@@ -20,10 +20,10 @@ buildGoModule {
     in
     ''
       mv $out/bin/${subPackage} $out/bin/${pname}
-      installShellCompletion --cmd ${pname} \
-        --bash <(${emulator} $out/bin/${pname} completion bash) \
-        --fish <(${emulator} $out/bin/${pname} completion fish) \
-        --zsh  <(${emulator} $out/bin/${pname} completion zsh)
+      # installShellCompletion --cmd ${pname} \
+      #   --bash <(${emulator} $out/bin/${pname} completion bash) \
+      #   --fish <(${emulator} $out/bin/${pname} completion fish) \
+      #   --zsh  <(${emulator} $out/bin/${pname} completion zsh)
     '';
   vendorHash = "sha256-1ZiskvF401IQ9qzNHCJG2F3TohNxl3+zEv+cU1kbBWo=";
 }
